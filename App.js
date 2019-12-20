59a0338a10994f55fe23a4c4e9d807b96ed9793c
JayJay-H <0911aza@naver.com>
2019-12-20 17:05:08 +0900

    #1.4

--- App.js
+++ App.js
@@ -2,17 +2,27 @@ import React from 'react';
 import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
+import axios from "axios";
+
+const API_KEY = "03cb05608b9619bbdc253f4c047d0339";
 
 export default class extends React.Component {
   state = {
     isLoading: true
   };
+  getWeather = async(latitude, longitude) => {
+    const { data } = await  axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+      );
+    console.log(data);
+  }
   getLocation = async() => {
     try {
       await Location.requestPermissionsAsync();
       const { 
         coords: { latitude, longitude } 
       } = await Location.getCurrentPositionAsync();
+      this.getWeather( latitude, longitude );
       this.setState({isLoading: false});
       // 받은 값들을 API로 보내서 날씨를 받아올거야!
     } catch (error) {
